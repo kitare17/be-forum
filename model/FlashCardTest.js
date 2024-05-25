c75d7592e48854619ac325ab8a1d0143ff5d73b5
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-05-26 01:00:56 +0700

    Updated

--- model/FlashCardTest.js
+++ model/FlashCardTest.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 
-const flashCardTestSchema = new mongoose.Schema({
-  testId: { type: mongoose.Schema.Types.ObjectId, ref: "Test", required: true },
-  flashCardId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Flashcard",
-    required: true,
+const flashCardTestSchema = new mongoose.Schema(
+  {
+    testId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Test",
+      required: true,
+    },
+    flashCardId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Flashcard",
+      required: true,
+    },
+    userAnswer: { type: String, required: true },
   },
-  userAnswer: { type: String, required: true },
-  created_at: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("FlashCardTest", flashCardTestSchema);
